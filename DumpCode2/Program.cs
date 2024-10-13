f1f6c687139f054424c4fbeddebbc186ab77ea12
Dennis <dennisbircan@gmail.com>
2024-10-13 19:49:05 +0200

    Ext 1

---
+++ DumpCode2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata;
+using System;
+
+namespace DumpCode2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Create a simple C# program that calculates the average temperature from an array of temperatures. The program should:
+        //Declare a method CalculateAverage that takes an array of doubles as a parameter.
+        //Implement the method to calculate the average of the temperatures in the array.
+        //Declare a method PrintAverage that takes an array of doubles as a parameter.
+        //Implement the method to call CalculateAverage and print the result to the console.
+        //Alert!
+        //The result of execution for the default string should be:
+        //"The average temperature is: 23.5"
+        }
+
+        public void PrintAverage(double[] temperatures)
+        {
+            Console.WriteLine($"The average temperature is: {CalculateAverage(temperatures)}");
+        }
+
+        public double CalculateAverage(double[] temperatures)
+        {
+            double avgTemp = 0.0;
+            foreach (double d in temperatures)
+            {
+                avgTemp += d;
+            }
+            avgTemp /= temperatures.Length;
+            return avgTemp;
+        }
+    }
+}
---
+++ Ext1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ext1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "23a";
+            bool number1 = name.IsNumeric(out int number);
+
+            Console.WriteLine(number1);
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+
+    }
+    public static class MyExtensions
+    {
+        public static bool IsNumeric(this string s, out int number)
+        {
+            return int.TryParse(s, out number);
+        }
+    }
+}
